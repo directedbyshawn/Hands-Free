90bc5f1d6636b1e729d90f6cacd9c3ccaa6862be
Shawn Rodgers <me@directedbyshawn.com>
2022-10-13 15:03:28 -0400

    created files

---
+++ run.py
@@ -0,0 +1,13 @@
+'''
+
+    Driver for autonomous vehicle image classification.
+
+'''
+
+from models.obstacles import ObstacleDetector
+
+def main():
+    print("test")
+
+if __name__ == '__main__':
+    main()

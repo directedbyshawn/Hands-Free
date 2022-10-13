e1b18ed8f311955b5c141f791113539ce1d495eb
david <585das@gmail.com>
2022-10-13 15:09:23 -0400

    header comments and imports

---
+++ road_detection.py
@@ -0,0 +1,11 @@
+'''
+	ML Group 8
+	The goal of this portion of the project is focused on detecting edges of the road.
+	This may be extended to lane detection, rather than just road edgeline detection.
+	The ability to detect where the edges of the road are or where one's lane is, is an
+	important component of autonomous driving. It is the core of having a car drive on its
+	own.
+'''
+import numpy as np
+import scikitlearn as sk
+import tensorflow as tf
